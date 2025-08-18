c0a53ea8f100a75982ff436f94820994702a422f
gustavodiasag <gustavodias.aguiar1@gmail.com>
2025-08-18 19:35:01 -0300

    feat: class declarations

--- grammar.js
+++ grammar.js
@@ -45,6 +45,69 @@ module.exports = grammar({
   ],
 
   rules: {
+    source_file: $ => seq(
+      repeat($.class_item),
+    ),
+
+    class_item: $ => seq(
+      'class',
+      field('name', $._type_identifier),
+      optional(field('inherits', $.inherits)),
+      field('features', $.field_declaration_list),
+      ';',
+    ),
+
+    inherits: $ => seq(
+      'inherits',
+      $._type_identifier,
+    ),
+
+    field_declaration_list: $ => seq(
+      '{',
+      repeat(
+        choice(
+          $.attribute_declaration,
+          $.method_declaration,
+        ),
+      ),
+      '}',
+    ),
+
+    attribute_declaration: $ => seq(
+      field('name', $identifier),
+      ':',
+      field('type', $._type_identifier),
+      ';',
+    ),
+
+    method_declaration: $ => seq(
+      field('name', $.identifier),
+      field('parameters', $.parameters),
+      ':',
+      optional(seq(
+        ':',
+        field('return_type', choice($._type_identifier, self_type)))
+      ),
+      field('body', $.block),
+    ),
+
+    parameters: $ => seq(
+      '(',
+      sepBy(',', $.parameter),
+      ')',
+    ),
+
+    parameter: $ => seq(
+      field('name', $identifier),
+      ':',
+      field('type', $._type_identifier),
+    ),
+
+    block: $ => seq(
+      '{',
+      repeat($._expression),
+      '}',
+    ),
 
     comment: $ => choice(
       $.inline_comment,
@@ -68,3 +131,27 @@ module.exports = grammar({
     ),
   },
 });
+
+/**
+ * Creates a rule to match one or more of the rules separated by the separator.
+ *
+ * @param {RuleOrLiteral} sep - The separator to use.
+ * @param {RuleOrLiteral} rule
+ *
+ * @returns {SeqRule}
+ */
+function sepBy1(sep, rule) {
+  return seq(rule, repeat(seq(sep, rule)));
+}
+
+/**
+ * Creates a rule to optionally match one or more of the rules separated by the separator.
+ *
+ * @param {RuleOrLiteral} sep - The separator to use.
+ * @param {RuleOrLiteral} rule
+ *
+ * @returns {ChoiceRule}
+ */
+function sepBy(sep, rule) {
+  return optional(sepBy1(sep, rule));
+}
